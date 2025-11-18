955e959db217a0054848149ce024f676945cab79
isaacodofin <eminentboy12345@gmail.com>
2025-11-18 14:09:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ app.get('/code', async (req, res) => {
                     console.log(`🔐 Requesting pairing code for: ${num}`);
                     
                     // ✅ CUSTOM BRANDING (like June MD)
-                    const custom = "GIFT-MD";
+                    const custom = "GIFT-PAIR";
                     const code = await sock.requestPairingCode(num, custom);
                     
                     console.log(`✅ Pairing code generated: ${code}`);
